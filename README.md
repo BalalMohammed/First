49ed5a6662c9b3b04194d8d9d6be3abca2c27a0c
Mohammed <B.Mohammed2489@student.leedsbeckett.ac.uk>
2025-01-29 13:34:45 +0000

    newcode

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # First
 First Programming class of 2025
+
+Welcome To LBU
---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 3
+
+#if __name__ == "__main__":
+    #print ("Hello, LBU!!!")
+
+name = input("What Is Your Name? ")
+
+if name == "Balal":
+    print ("You Are Balal")
